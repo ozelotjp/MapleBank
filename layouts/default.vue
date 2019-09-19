ed6e5644a1f3c274ed156a4702fd12a7a83139c2
ozelotjp <contact@ozelot.app>
2019-09-19 10:02:51 +0900

    wip

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  v-app
+    v-app-bar(app)
+      v-toolbar-title まるっち銀行
+      v-spacer
+      v-toolbar-items
+        v-btn(icon): v-icon mdi-google-spreadsheet
+        v-btn(icon): v-icon mdi-send
+    v-content
+      nuxt
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-container
+    v-btn(@click="submit()") submit
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      price: 0,
+      description: 'テスト'
+    }
+  },
+  methods: {
+    submit() {
+      const price = this.price
+      const description = this.description
+      window.open(
+        `https://docs.google.com/forms/d/e/1FAIpQLSd1upHJp_E_OnR22TGd_LSXmjUaaGxfyrEKW18uTi7h-g_GHw/formResponse?entry.741369983=${price}&entry.439718942=${description}`
+      )
+    }
+  }
+}
+</script>
