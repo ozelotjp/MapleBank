5ed44a9e75800c9b0e38f6eed1118fde7e160b60
ozelotjp <contact@ozelot.app>
2019-09-21 00:22:29 +0900

    wip

--- pages/view.vue
+++ pages/view.vue
@@ -35,7 +35,7 @@ export default {
       this.transactions.forEach((element) => {
         const date = element.date.toDate()
         const y = date.getFullYear()
-        const m = ('0' + date.getMonth()).slice(-2)
+        const m = ('0' + (date.getMonth() + 1)).slice(-2)
         const d = ('0' + date.getDate()).slice(-2)
         const h = ('0' + date.getHours()).slice(-2)
         const i = ('0' + date.getMinutes()).slice(-2)
